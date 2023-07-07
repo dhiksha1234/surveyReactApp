8f16999c94ae9e502ce867ebe71f0a29bd964ed1
dhiksha1234 <dhiksha.sivaraman@crystaldelta.com>
2023-07-07 15:54:38 +0530

    Completed the form UI

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,78 @@
-import Image from 'react-bootstrap/Image';
-import Row from 'react-bootstrap/Row';
+import { Navbar , Image } from 'react-bootstrap';
+ 
+ 
+const surveyQuestions = [
+  {
+    "surveyQuestion":"I am confident that I will successfully complete this course",
+    "type":"radioButton"
+  },
+  {
+    "surveyQuestion":"You are a fast learner",
+    "type":"radioButton"
+  },
+  {
+    "surveyQuestion":"You are a interested in Maths",
+    "type":"radioButton"
+  },
+  {
+    "surveyQuestion":"You are a interested in Science",
+    "type":"radioButton"
+  },
+  {
+    "surveyQuestion":"You are a interested in Social",
+    "type":"radioButton"
+  }
+ ]
+
+ const questionOptions = ['Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree']
+
+ 
 
 
 function Form() {
     return(
         <>
-        <h1> Form </h1>
-        <Row>
-             <div>
+             <div className='image-wrapper'>
              <Image src="./group_study.jpg" className=''   width="100%"
               height="400" style={{ objectFit:'cover'}}/>
             </div>
-          </Row>
+            <Navbar className='d-flex justify-content-between navBar' data-bs-theme="dark">
+              <Navbar.Brand href="#home" >Page 1 of 3</Navbar.Brand>
+              <Navbar.Brand href="#home" > 4% completed</Navbar.Brand>
+            </Navbar>
+            <div className='question mt-5 mx-5'>
+               
+             { surveyQuestions.map((question,index)=>{
+            return (
+              <>
+              <h3 className='textColor fw-bold mt-4' key={index}>{question.surveyQuestion}</h3>
+              {
+                questionOptions.map((questionOption,index) =>{
+                   return(
+                   <div className='form-check radioBtn'>
+                    <input className='form-check-input mx-3' type="radio" name={questionOption} key={index} id={index} onClick={(event)=>{console.log('click',event.target.name,event,question.index)}}/>
+                    <label className="form-check-label" htmlFor="flexRadioDefault1">
+                      {questionOption}
+                    </label>
+                  </div>
+                  )
+                })
+              }
         </>
+        )
+      })}
+
+            </div>
+
+            <hr/>
+            <div className='pageFooter d-flex justify-content-end'> 
+              
+             <button type="button" className = "btn mt-2 mx-5 saveLaterBtn mb-2"> Save for later</button>
+             <button type="button" className = "btn mt-2 mx-3 startedBtn mb-2"> Next </button>
+
+            </div>
+
+         </>
     )
 }
 
--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -14,36 +14,45 @@ function WelcomePage(){
             </div>
           </Row>
           <Row mt='4'>
-            <div className='display-3 text-center fw-bold mt-4 welcomeTitle'>
+            <div className='display-3 text-center mt-4 textColor'>
             Welcome to your Personal Learning Profile
             </div>
              
-            <Col className='mt-4 mx-4 welcomeText'>
+            <Col className='mt-4 mx-4'>
             Welcome All
-            <br/>
+          
             <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
-            <br/>
+          
             <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. </p>
-             <br/>
+             
             <p>The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-             <br/>
+        
             <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-            <br/>
+      
             <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-            <br/>
             <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. </p>
+            <p>We look forward to provide your learning profile</p>
 
             </Col>
            </Row>
                     
-             
-            <div>
-            <Link to="/form"><button type="button" className = "btn btn-warning btn-sm btn-rounded mt-5 mx-5 startedBtn"> Get Started </button></Link>
+             <div>
+            <Link to="/form"><button type="button" className = "btn btn-warning btn-sm btn-rounded mt-2 mx-5 startedBtn"> Lets get Started </button></Link>
             </div>
 
             <hr/>
             <Row className='mx-4'>
-            <h1> Our support team</h1>
+            <h1 className="textColor"> Our support team</h1>
+
+            <p>Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.  </p>
+
+            <ul className='mx-5'>
+              <li>Latin professor at Hampden-Sydney College</li>
+              <li>Apply below for those interested. Sections 1.</li>
+              <li>Sydney College in Virginia, looked up </li>
+               
+            </ul>
+
 
             <p>Email: student.success@rmit.edu.co</p>
             <p>Call: 987654321</p>
