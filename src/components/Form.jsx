339891068e82792fe90048205375fc9db9b6106d
dhiksha1234 <dhiksha.sivaraman@crystaldelta.com>
2023-07-12 15:59:44 +0530

    Pagination was added using react-paginate

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import { Navbar , Image } from 'react-bootstrap';
 import { useState, useEffect } from 'react';
- 
-
+import AlertModal from './alertModal';
+import ReactPaginate from 'react-paginate';
   
 const surveyQuestions = [
   {
@@ -65,134 +65,129 @@ const surveyQuestions = [
     "type":"radioButton"
   }
  ]
-
  const questionOptions = ['Strongly Disagree','Disagree','Neutral','Agree','Strongly Agree']
-
+ 
   
-
 function Form() {
   const [ pageNumber, setPageNumber] = useState(1);
   const [ selectedRadioBtn, setSelectedRadioBtn] = useState(0);
   const [ answeredQuestion, setAnsweredQuestion] = useState({});
   const [ firstIndex, setFirstIndex] = useState(0);
-  const [ lastIndex, setLastIndex] = useState(5);
-  const [ backButtonEnable, setBackButtonEnable] = useState(false);
-  const [ question, setQuestion] = useState(surveyQuestions.slice(0,5));
-  const [ reachedEnd, setReachedEnd]= useState(false);
+  const [ allAnsweredPerPage, setAllAnsweredPerPage] = useState(false);
+
   const perPage = 5;
   const totalPageCount = Math.ceil(surveyQuestions.length / perPage);
-
  
   const handleOnChange = (index,question,e) =>{
+    console.log(e.target.value,e.target.name)
     setSelectedRadioBtn(index);
+    console.log(selectedRadioBtn)
     setAnsweredQuestion({...answeredQuestion, 
       [e.target.name] : e.target.value})
     }
 
-   const handleNextPage = () =>{
-    if(totalPageCount === pageNumber){
-      console.log("end")
-      setReachedEnd(true)
-    }
-    console.log("Next button before setting -- firstIndex",firstIndex,"lastIndex",lastIndex,"pageNumber",pageNumber)
-    setBackButtonEnable(true)
-    setPageNumber(pageNumber+1)
-    setLastIndex(5*pageNumber)
-
-    setFirstIndex(lastIndex)
-    setQuestion(surveyQuestions.slice(firstIndex,lastIndex))
-    
-    console.log("Next button after -- firstIndex",firstIndex,"lastIndex",lastIndex,"pageNumber",pageNumber)
-    console.log("question", question);
+  const handleNextPage = () =>{
+     setPageNumber((pageNumber) => pageNumber+1 )
+   
+  }
  
+  const lastIndex = firstIndex+perPage;
+
+  const PreviousBtn = () => {
+    return(
+      <button type="button" className = "btn mt-2 saveLaterBtn mb-2" onClick={()=>{handlePreviousPage()}}> Previous</button>
+    )
+   }
+   const NextBtn = () => {
+    return(
+      <button type="button" className = "btn mt-2 startedBtn mb-2" onClick={()=>{handleNextPage()}}> Next </button>
+     )
+   }
+   const SubmitBtn = () =>{
+    return(
+      <button type="button" className = "btn  mt-2 startedBtn mb-2" > Submit </button>
+    )
+   }
+   const handlePreviousPage = () =>{
+     setPageNumber(pageNumber-1)
   }
 
- 
-
-   const handlePreviousPage = () =>{
-    console.log("Previous button after -- firstIndex",firstIndex,"lastIndex",lastIndex,"pageNumber",pageNumber)
-
-    setPageNumber(pageNumber-1)
-    setLastIndex(5*pageNumber)
-    setFirstIndex(lastIndex-5)
-    setQuestion(surveyQuestions.slice(firstIndex,lastIndex))
-
-    console.log("Previous button after -- firstIndex",firstIndex,"lastIndex",lastIndex,"pageNumber",pageNumber)
-  
+   const changePage = ({selected}) =>{
+    const newFirst = (selected*perPage) %surveyQuestions.length
+    setFirstIndex(newFirst);
    }
-
-  useEffect(()=>{
-    // console.log('Q and A',answeredQuestion);
-    // setQuestion(surveyQuestions.slice(firstIndex,lastIndex))
-    // console.log("firstIndex", firstIndex)
-    // console.log("lastIndex",lastIndex)
-    // console.log("pageNo",pageNumber)
-    // setFirstIndex(lastIndex)
-      // setFirstIndex(lastIndex-5)
-      // setLastIndex(5*pageNumber)
-      // setQuestion(surveyQuestions.slice(firstIndex,lastIndex))
-     // console.log("effect - selectedRadioBtn",selectedRadioBtn);
-   }, [selectedRadioBtn, answeredQuestion,firstIndex,lastIndex,pageNumber])//what to execute, when
 
     return(
         <>
-             <div className='image-wrapper'>
+            {/* Image */}
+            <div className='image-wrapper'>
              <Image src="./group_study.jpg" className=''   width="100%"
               height="400" style={{ objectFit:'cover'}}/>
             </div>
+            {/* Navbar */}
             <Navbar className='d-flex justify-content-between navBar' data-bs-theme="dark">
               <Navbar.Brand >Page {pageNumber} of {totalPageCount}</Navbar.Brand>
               <Navbar.Brand > First Index {firstIndex} Last Index {lastIndex}</Navbar.Brand>
               <Navbar.Brand > {Math.floor(Object.keys(answeredQuestion).length*6.7)}% completed</Navbar.Brand>
              </Navbar>
-
              {/* Mapping the surveyQuestions */}
             <div className='question mt-5 mx-5'>
-              { question.map((question,parentIndex)=>{
-               
-            return (
-               <>
-              <div className='question-wrapper' key={parentIndex}>
-              <h3 className='textColor fw-bold mt-4'>{question.surveyQuestion}</h3>
-                {/* Mapping the options */}
-               {
-                 questionOptions.map((option,index) =>{
-                  return(
-                      <div className ='radioBtn' > 
-                      <input 
-                      className='form-check-input mx-3' 
-                      type="radio" name={question.surveyQuestion} 
-                      value={option}
-                      onChange={(e)=>{
-                        handleOnChange(index,question.surveyQuestion,e)
-                        }}
-                        />
-                      <label className="form-check-label" htmlFor={option.question}>
-                          {option}
-                      </label>
-                      </div>
+              { surveyQuestions.slice(firstIndex,lastIndex).map((question,parentIndex)=>{
+                
+                  return (
+                    <>
+                    <div className='question-wrapper' key={parentIndex}>
+                    <h3 className='textColor fw-bold mt-4'>{question.surveyQuestion}</h3>
+                      {/* Mapping the options */}
+                    {
+                      questionOptions.map((option,index,e) =>{
+                        
+                        return(
+                            <div className ='radioBtn' > 
+                            <input 
+                            className='form-check-input mx-3' 
+                            type="radio" name={question.surveyQuestion} 
+                             
+                            value={option}
+                            id={`${question.surveyQuestion}_${option}`}
+                            onChange={(e)=>{
+                              handleOnChange(index,question.surveyQuestion,e)
+                              }}
+                              />
+                            <label className="form-check-label" htmlFor={`${question.surveyQuestion}_${option}`}>
+                              {option}
+                            </label>
+                            </div>
+                            
+                            )
+                            })
+                          }
+                        </div>
+                      </>
                       )
-                      })
-                    }
-                  </div>
-                </>
-                )
-              })}
-
+                    })}
             </div>
-
             <hr/>
+            <AlertModal allAnsweredPerPage={allAnsweredPerPage} setAllAnsweredPerPage={setAllAnsweredPerPage}/>
+            <div className='pageFooter '>
+            <ReactPaginate
+              previousLabel={pageNumber===1?'':<PreviousBtn/>}
+              nextLabel={totalPageCount === pageNumber ?<SubmitBtn/> : <NextBtn/>}
+              pageCount={totalPageCount}
+              onPageChange={changePage}
+              containerClassName={"paginationBtn"}
+              previousLinkClassName={"pre"}
+              nextLinkClassName={"nextLink"}
+              disabledClassName={"disable"}
+              activeClassName={"paginationActive"}
+            />
+     
             
-            <div className='pageFooter d-flex justify-content-end'>
-              {backButtonEnable && <button type="button" className = "btn mt-2 mx-5 saveLaterBtn mb-2" onClick={()=>{handlePreviousPage()}}> Previous</button>}
-              
-              <button type="button" className = "btn mt-2 mx-5 saveLaterBtn mb-2 " > Save for later</button>
-              {reachedEnd || <button type="button" className = "btn mt-2 mx-3 startedBtn mb-2" onClick={()=>{handleNextPage()}}> Next </button>}
-              {reachedEnd && <button type="button" className = "btn mt-2 mx-3 startedBtn mb-2" > Submit </button>}
-            </div>
-
+               
+              </div>
+             <button type="button" className = "btn mt-2 mx-5 saveLaterBtn mb-2 " > Save for later</button>
+          
          </>
     )
 }
-
 export default Form;
